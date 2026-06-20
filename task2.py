e744d57a5983c2e932cec484e94fda8fc1260459
Balu-Palle <pallebalu18@gmail.com>
2026-06-21 01:40:26 +0530

    task2 updated

--- task2.py
+++ task2.py
@@ -6,3 +6,15 @@ for i in range(len(a)):
         if i <j:
             a[i][j],a[j][i]=a[j][i],a[i][j]
 print(a)
+
+
+# matrix add,sub, mul using numpy
+
+# import numpy as np
+
+# a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# b=np.array([[9,8,7],[6,5,4],[3,2,1]])
+
+# print(a+b)
+# print(a-b)
+# print(np.matmul(a,b))

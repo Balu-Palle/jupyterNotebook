02dcbd6fa865fea30ec92a87a351fed55c3ad9e3
Balu-Palle <pallebalu18@gmail.com>
2026-06-24 01:37:35 +0530

    updated task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,15 @@
 #Simple neuron class with bias,forward,weights
+
+import numpy as np
+
+class SimpleNeuron:
+    def __init__ (self,weights,bias):
+        self.weights=np.array(weights)
+        self.bias=bias
+    def forward(self,weights,bias,inputs):
+        inputs=np.array(inputs)
+        return np.dot(inputs,self.weights) + self.bias
+    
+s=SimpleNeuron(weights=[0.2,0.3,0.4],bias=1)
+
+print(s.forward(inputs=[5,4,3]))

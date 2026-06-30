7c5128fe105787d5c56ec33a06e95cb7c67e81e7
Balu-Palle <pallebalu18@gmail.com>
2026-07-01 00:33:35 +0530

    task4 updated

---
+++ task4.py
@@ -0,0 +1,3 @@
+#Gradient descent is how neural networks learn. It asks: "which direction should I nudge my 
+# weights to reduce the error?" The answer is always 
+# the negative of the derivative. Today you implement this on the simplest possible function — y = x² — where the minimum is obviously at x = 0. You will watch the algorithm find it automatically.
